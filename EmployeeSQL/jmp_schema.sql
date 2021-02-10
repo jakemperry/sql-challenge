1ffe13f701aa090dad6fac635965d595f18ffa52
Jake Perry <jakemperry@gmail.com>
2021-02-09 22:39:25 -0800

    update schema

--- EmployeeSQL/jmp_schema.sql
+++ EmployeeSQL/jmp_schema.sql
@@ -1,20 +1,32 @@
+--Create Pewlett Hackard database
+create database Pewlett_Hackard_db;
+
 --Create Departments Table
 create table departments (
 	dept_no VARCHAR(4) not null,
 	dept_name VARCHAR(30) not null
 );
+	
+--Confirm successful Departments import
+select * from departments;
 
 --Create Dept_Emp Table
 create table dept_emp(
 	emp_no int not null,
 	dept_no VARCHAR(4) not null
 );
+
+--Confirm successful Dept_Emp import
+select * from dept_emp;
 	
 --Create Dept_Manager Table
 create table dept_manager(
 	dept_no VARCHAR(4) not null,
 	emp_no int
 );
+
+--Confirm successful Dept_Manager import
+select * from dept_manager;
 	
 --Create Employees Table
 create table employees(
@@ -27,14 +39,23 @@ create table employees(
 	hire_date VARCHAR(10)
 );
 
+--Confirm successful Employees import
+select * from employees;
+
 --Create Salaries Table
 create table salaries(
 	emp_no int,
 	salary int
 );
 
+--Cofirm successful Salaries import
+select * from salaries;
+
 --Create Titles Table
 create table titles(
 	title_id VARCHAR(5),
 	title VARCHAR(30)
 );
+
+--Confirm successful Titles import
+select * from titles;
